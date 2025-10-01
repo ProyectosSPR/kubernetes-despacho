a2452ec53a1ad0caa4737a74e7bce38305934c70
sergio perez <eperez@dml-medica.com>
2025-10-01 16:11:35 -0600

    eliminacion odoo 17

--- odoo/addons-extra/sttl_yodlee_integration/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_config
-from . import yodlee_account
-from . import yodlee_bank
--- odoo/addons-extra/sttl_yodlee_integration/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import yodlee_transaction_wizard
